095bcb80295eb9fa796ace98d0d3028795c73d74
kruemmelbande <kai.scheufen@gmail.com>
2023-03-22 06:44:46 +0100

    Changed folder name and made it save .ogg

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ This tool reads a beatsaber custom song folder and then pulls all the songs into
 
 ### Extracting from PC:
 Go into the script and disable pullDataFromQuest.  
-Put your beatmaps in a folder named QuestMaps (will be changed later) in the same path as this script, then execute it.  
+Put your beatmaps in a folder named BeatMaps in the same path as this script, then execute it.  
 
 ### Extracting from Quest:
 Make sure you have adb in your path, then execute this script.  
-Optionally you can delete the QuestMaps folder afterwards.  
+Optionally you can delete the BeatMaps folder afterwards.  
 
 ## Known issues:  
 Sometimes it fails to copy files correctly and sometimes it copies them with weird filenames
--- beatSongPuller.py
+++ beatSongPuller.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import json
 
-pullDataFromQuest = True
+pullDataFromQuest = False
 
 # Step 1: List all the custom levels in the SongLoader folder on the Quest using adb
 if pullDataFromQuest:
@@ -11,15 +11,15 @@ if pullDataFromQuest:
 
 # Step 2: Pull all the custom levels from the Quest using adb
 
-    os.makedirs("QuestMaps", exist_ok=True)
+    os.makedirs("BeatMaps", exist_ok=True)
     for folder in folders:
         src_folder = f"/sdcard/ModData/com.beatgames.beatsaber/Mods/SongLoader/CustomLevels/{folder}"
-        dst_folder = f"QuestMaps/{folder}"
+        dst_folder = f"BeatMaps/{folder}"
         subprocess.run(["adb", "pull", src_folder, dst_folder])
 
 # Step 3: Read all the song names and author names from the info.dat files of each map
 songs = []
-for root, dirs, files in os.walk("QuestMaps"):
+for root, dirs, files in os.walk("BeatMaps"):
     for file in files:
         if file.endswith("info.dat") or file.endswith("Info.dat"):
             with open(os.path.join(root, file), "r", encoding="utf8") as f:
@@ -36,7 +36,7 @@ os.makedirs("Playlist", exist_ok=True)
 for song in songs:
     try:
         src_path = os.path.join(song[0], "song.egg")
-        dst_path = os.path.join(".\\Playlist", f"{song[1]} - {song[2]}.egg")
+        dst_path = os.path.join(".\\Playlist", f"{song[1]} - {song[2]}.ogg")
         #subprocess.run(["cp", f'{src_path}', f'{dst_path}'])
         command = f'copy "{src_path}" "{dst_path}"'
         #print(command)
