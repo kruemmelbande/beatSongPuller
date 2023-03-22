c605e5b3de01d8867a172d2289e2e271a5f1ba90
kruemmelbande <kai.scheufen@gmail.com>
2023-03-22 14:26:34 +0100

    more user feedback added

--- beatSongPuller.py
+++ beatSongPuller.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import json
 
-pullDataFromQuest = False
+pullDataFromQuest = True
 
 if pullDataFromQuest:
     # Step 1: List all the custom levels in the SongLoader folder on the Quest using adb
@@ -29,22 +29,30 @@ for root, dirs, files in os.walk("BeatMaps"):
                     song = info["_songName"]
                     author = info["_songAuthorName"]
                     filename=info["_songFilename"]
-                    print(f"Found {song} by {author}")
                     songs.append((root, song, author, filename))
+                    print(f"Found {song} by {author}")
+                    break
                 except:
+                    print("An error occured when reading a beatmap:")
                     print(info)
+            break
 # Step 4: Save all the songs into a folder with the correct names
 os.makedirs("Playlist", exist_ok=True)
+i=0
 for song in songs:
     try:
         src_path = os.path.join(song[0], song[3])
         dst_path = os.path.join(".\\Playlist", f"{song[1]} - {song[2]}.ogg")
-        #subprocess.run(["cp", f'{src_path}', f'{dst_path}'])
+        if os.path.exists(dst_path):
+            continue
         command = f'copy "{src_path}" "{dst_path}"'
         #print(command)
+        #subprocess.run(["copy", f'"{src_path}"', f'"{dst_path}"']) # This doesn't work for some reason
         os.system(command)
+        i+=1
     except Exception as e:
         print(f"Failed to copy {song[1]} - {song[2]}: {e}")
         print(f"Source: {src_path}")
         print(f"Destination: {dst_path}")
-        
+actualSongs = os.listdir("Playlist")
+print(f"{len(actualSongs)} songs were copied, {len(songs)-len(actualSongs)} songs encountered errors or were duplicates.")
